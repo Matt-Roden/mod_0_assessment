671a8525ca1b1d076be062b09ca30d95896bd392
Matt Roden <mattroden92@gmail.com>
2021-04-22 13:59:01 -0600

    Add data to modelB.js

---
+++ models/modelB.js
@@ -0,0 +1,12 @@
+var vehicle = {
+  make: "Ford",
+  model: "Fusion",
+  miles: 45000,
+  numOfDoors: 4,
+  isDamaged: false,
+  isClean: true,
+  features: ["Bluetooth stereo", "All wheel drive", "Backup camera system"],
+  bodyMaterials: ["sheet metal", "plastic", "chrome"]
+}
+
+console.log(vehicle);
