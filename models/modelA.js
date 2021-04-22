368f77a0e2984b29b5e69464002e041c984173ca
Matt Roden <mattroden92@gmail.com>
2021-04-22 13:55:35 -0600

    Create modelA.js and modelB.js and add data to modelA.js

---
+++ models/modelA.js
@@ -0,0 +1,12 @@
+var airport = {
+  name: "Denver International Airport",
+  nickname: "DIA",
+  numOfTerminals: 3,
+  numOfGates: 100,
+  isMassive: true,
+  isHaunted: true,
+  terminalNames: ["Terminal A", "Temrinal B", "Temrinal C"],
+  conspiracies: ["Lizard people", "Underground tunnels", "Bluecifer"]
+}
+
+// console.log(airport);
